3cea6f7d74aeb4d90c4c1ee0d68ef2f9bdb43293
subbak2 <subbak2@likelion.org>
2022-01-11 23:55:33 +0900

    python 따릉이 API 220111

---
+++ 2201/test.py
@@ -0,0 +1,35 @@
+import requests
+import logging
+import http.client as http_client
+import json
+
+try:
+    import http.client as http_client
+except ImportError:
+    # Python 2
+    import httplib as http_client
+http_client.HTTPConnection.debuglevel = 0
+
+# 로그용
+# logging.basicConfig()
+# logging.getLogger().setLevel(logging.DEBUG)
+# requests_log = logging.getLogger("requests.packages.urllib3")
+# requests_log.setLevel(logging.DEBUG)
+# requests_log.propagate = True
+
+# URL : http://openapi.seoul.go.kr:8088/{인증키}/json/bikeList/1/5/
+genReqUrl = 'http://openapi.seoul.go.kr:8088/{본인인증키}/json/bikeList/1/5/'
+
+data = requests.get(genReqUrl)
+result = json.loads(data.text)
+
+# print(result)
+
+rows = result["rentBikeStatus"]["row"]
+
+for i in rows:
+    stationName = i["stationName"]
+    rackTotCnt = i["rackTotCnt"]
+    parkingBikeTotCnt = i["parkingBikeTotCnt"]
+
+    print("대여소 이름 : "+stationName+", 거치대 개수 : "+rackTotCnt+", 주차된 자전거 수 : "+parkingBikeTotCnt)
